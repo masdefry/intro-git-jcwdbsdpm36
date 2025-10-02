b268e096a9353efecab35928b9f72b5454c23aca
haidarfahmii <haidarfahmi2000@gmail.com>
2025-10-02 15:33:00 +0700

    feat(profile-page): add console.log

---
+++ profile.js
@@ -0,0 +1,9 @@
+console.log("------ PROFILE ------");
+console.log("Name: Adrian");
+console.log("Age: 27 Tahun");
+console.log("No HP: 085916482210");
+console.log("Addres: Jakarta Selatan");
+console.log("Skill: JavaScript, Cotlin, Python, Web Devloper, Data Science");
+console.log("---------------------")
+
+
